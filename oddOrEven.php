d71e8adc22174b9ffe5493a85c562e1d7aeff574
Alexander-Maldonado <alex_ahc@yahoo.com>
2023-05-05 01:20:35 -0700

    odd or  even arrangment in php

---
+++ oddOrEven.php
@@ -0,0 +1,12 @@
+<?php
+$array = [];
+$even_number = 2;
+
+for ($i = 1; $i < 2001; $i++) {
+    if ($i % $even_number != 0) { //checking to see that the integer is not divisible by 2
+        echo 'Number is '. $i.'.' .'This is an odd number. <br>';
+    } else { // if it is than it knows its an  even number
+        echo 'Number is '. $i.'.' .'This is an even number. <br>';
+    }
+}
+?>
